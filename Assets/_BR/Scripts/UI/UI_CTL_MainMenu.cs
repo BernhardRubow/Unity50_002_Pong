a34ca1d20e583bdc8043647d2ae8002da6812ed3
Bernhard Rubow <brubow@newvisionsproject.de>
2019-02-22 13:10:37 +0100

    Added dotween

--- Assets/_BR/Scripts/UI/UI_CTL_MainMenu.cs
+++ Assets/_BR/Scripts/UI/UI_CTL_MainMenu.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using nvp.interfaces;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
+using DG.Tweening;
 
 public class UI_CTL_MainMenu : MonoBehaviour
 {
     
     // +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     private IEventController _eventController;
+    [SerializeField] private TextMeshProUGUI _pongText;
 
     // +++ life cycle +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     [Inject]
@@ -19,18 +23,23 @@ public class UI_CTL_MainMenu : MonoBehaviour
 
     void Start()
     {
-        
+        _pongText.transform
+            .DOScale(1.2f,20)
+            .SetEase(Ease.InOutCubic)
+            .SetLoops(-1, LoopType.Yoyo);
     }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.F1))
+        if (Input.GetKeyUp(KeyCode.Alpha1))
         {
+            Debug.Log("Starting One Player Game");
             _eventController.InvokeEvent(NvpGameEvents.OnStartOnePlayerGame, this, null);
         }
 
-        if (Input.GetKeyUp(KeyCode.F2))
+        if (Input.GetKeyUp(KeyCode.Alpha2))
         {
+            Debug.Log("Starting Two Player Game");
             _eventController.InvokeEvent(NvpGameEvents.OnStartTwoPlayerGame, this, null);
         }
     }
